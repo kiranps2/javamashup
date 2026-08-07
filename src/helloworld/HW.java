8d4ca82777c51818cf7d1b17a7ca6653bc56a2a8
kiran ps <kiranpsofficial1@gmail.com>
2026-08-07 10:25:20 +0530

    Add HW class to demonstrate basic output functionality

---
+++ src/helloworld/HW.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+public class HW {
+
+	public static void main(String[] args) {
+		String item="soap";
+		int quantity=4;
+		double unit_price=18.75;
+		double total=quantity*unit_price;
+		System.out.println("total:"+total);
+		System.out.println("item name:"+item);
+		System.out.println("quantity:"+quantity);
+		System.out.println("unit price:"+unit_price);
+
+	}
+
+}
