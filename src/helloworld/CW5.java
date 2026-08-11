838a04465d0be06e83de0744a6f968bfce578918
kiran ps <kiranpsofficial1@gmail.com>
2026-08-11 10:25:51 +0530

    Add CW5 class for user authentication with username and password

---
+++ src/helloworld/CW5.java
@@ -0,0 +1,23 @@
+package helloworld;
+import java.util.Scanner;
+public class CW5 {
+
+	public static void main(String[] args) {
+		String username="admin";
+		String psw="java123";
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the user name:");
+		String name=input.nextLine();
+		System.out.println("Enter the password:");
+		String pass=input.nextLine();
+		if (name.equals(username) && pass.equals(psw)) {
+			System.out.println("Login successful!!");
+		}else {
+			System.out.println("Access Denied!!");
+
+		}
+		
+
+	}
+
+}
