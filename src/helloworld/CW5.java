d230c5a53c7d51ea1fcf2f69d2bae0cc93d8fbef
kiran ps <kiranpsofficial1@gmail.com>
2026-08-12 10:07:29 +0530

    Add CW5 class to evaluate student marks and print performance categories

---
+++ src/helloworld/CW5.java
@@ -0,0 +1,24 @@
+package helloworld;
+
+public class CW5 {
+
+	public static void main(String[] args) {
+		int mark[]= {90,70,60,52,40};
+		for(int i=0;i<mark.length;i++) {
+			if(mark[i]>=90) {
+				System.out.println(mark[i]+":Excellent");
+			}else if(mark[i]>=75&&mark[i]<=89) {
+				System.out.println(mark[i]+":Good");
+
+			}else if(mark[i]>=50&&mark[i]<=74) {
+				System.out.println(mark[i]+":Average");
+
+			}else if(mark[i]<=50) {
+				System.out.println(mark[i]+":fail");
+
+			}
+		}
+
+	}
+
+}
