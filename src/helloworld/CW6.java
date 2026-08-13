39ae376971a1af24933c008e9675c690d24a3c6e
kiran ps <kiranpsofficial1@gmail.com>
2026-08-13 10:44:49 +0530

    Update CW6 class to handle out-of-stock scenarios and add HW6 class for grade evaluation

--- src/helloworld/CW6.java
+++ src/helloworld/CW6.java
@@ -3,11 +3,13 @@ package helloworld;
 public class CW6 {
 
 	public static void main(String[] args) {
-		int arr[]= {20, 15, 30, 10, 25};
+		int arr[]= {20, 15, 30, 0, 25};
 		int total=0;
+		int out=0;
 		for(int i=0;i<arr.length;i++) {
 			if(arr[i]==0) {
 				System.out.println("Out of stock");
+				out=1;
 				break;
 			}
 			
@@ -15,16 +17,20 @@ public class CW6 {
 				
 				total=total+arr[i];
 			}
-				if(total<50) {
-					System.out.println(total+":Low Stock");
-					
-				}else if(total<100) {
-					System.out.println(total+":Moderate Stock");
-					
-				}else if(total>=100) {
-					System.out.println(total+":Good Stock");
-					
-				}
+		if(out==0) {
+			if(total<50) {
+				System.out.println(total+":Low Stock");
+				
+			}else if(total<100) {
+				System.out.println(total+":Moderate Stock");
+				
+			}else if(total>=100) {
+				System.out.println(total+":Good Stock");
+				
+			}
+			
+		}
+				
 				
 			
 		}
---
+++ src/helloworld/HW6.java
@@ -0,0 +1,52 @@
+package helloworld;
+
+public class HW6 {
+
+	public static void main(String[] args) {
+		int arr[]= {78, 82, 91, 65, 36};
+		int total=0;
+		int avg=0;
+		int out=0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]<35) {
+				System.out.println("Fail");
+				out=1;
+				break;
+			}
+			
+			
+				
+				total=total+arr[i];
+				avg=total/arr.length;
+			}
+		if(out==0) {
+			if(avg>90) {
+				System.out.println("Grade: A:"+avg);
+				System.out.println("total:"+total);
+
+				
+			}else if(avg>75) {
+				System.out.println(avg+"Grade: B"+avg);
+				System.out.println("total:"+total);
+
+				
+			}else if(avg>60) {
+				System.out.println("Grade: C:"+avg);
+				System.out.println("total:"+total);
+
+				
+			}else if(avg<60) {
+				System.out.println("Grade: D:"+avg);
+				System.out.println("total:"+total);
+
+			}
+			
+		}
+				
+				
+			
+		}
+
+	}
+
+
