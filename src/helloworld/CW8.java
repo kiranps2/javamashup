4dd4696ae73fcf88ed8dce443151f331ed8d9579
kiran ps <kiranpsofficial1@gmail.com>
2026-08-17 10:45:24 +0530

    Add CW8 class implementing Animal interface with bird and dog subclasses

---
+++ src/helloworld/CW8.java
@@ -0,0 +1,37 @@
+package helloworld;
+
+public class CW8 {
+	interface Animal{
+		public void makeSound();
+		public void move();
+	}
+	static class bird implements Animal{
+		public void makeSound() {
+			System.out.println("Chirp");		
+			}
+		public void move() {
+			System.out.println("fly");
+		}
+	}
+	static class dog implements Animal{
+		public void makeSound() {
+			System.out.println("Bark");		
+			}
+		public void move() {
+			System.out.println("Run");
+		}
+	}
+	static class Zoo{
+		
+	public static void main(String[] args) {
+		
+			bird b=new bird();
+			dog d=new dog();
+			b.makeSound();
+			b.move();
+			d.makeSound();
+			d.move();
+		}
+	}
+
+}
