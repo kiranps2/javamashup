f740f234e7146434918f07da3fcc5e8e25cef8f8
kiran ps <kiranpsofficial1@gmail.com>
2026-08-21 09:10:22 +0530

    Add Student and StudentTest classes for managing student details and testing functionality

---
+++ src/DAY10/Student.java
@@ -0,0 +1,25 @@
+package DAY10;
+
+public class Student {
+	private String Name;
+	private int Rollno;
+	private double Persentage;
+	public String getName() {
+		return Name;
+	}
+	public int getRollno() {
+		return Rollno;
+	}
+	public double getPersentage() {
+		return Persentage;
+	}
+	public void setName(String n) {
+		this.Name=n;
+	}
+	public void setRollno(int r) {
+		this.Rollno=r;
+	}
+	public void setpersentage(double p) {
+		this.Persentage=p;
+	}
+}
---
+++ src/DAY10/StudentTest.java
@@ -0,0 +1,16 @@
+package DAY10;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student std=new Student();
+		std.setName("kiran");
+		std.setRollno(2);
+		std.setpersentage(87.45);
+		System.out.println("name: "+std.getName());
+		System.out.println("rollno: "+std.getRollno());
+		System.out.println("persentage: "+std.getPersentage());
+
+	}
+
+}
