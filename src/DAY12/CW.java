7060ee46b6fff3c6e343178e34dead167662fdd4
kiran ps <kiranpsofficial1@gmail.com>
2026-08-21 10:58:14 +0530

    Add CW class for Java file handling with read and write functionality

---
+++ src/DAY12/CW.java
@@ -0,0 +1,34 @@
+package DAY12;
+import java.io.*;
+
+public class CW {
+
+	public static void main(String[] args) {
+		File file=new File("assignment1.txt");
+		char[] array = new char[100];
+		String data="Java File Handling Practice";
+		try {
+			FileWriter output = new FileWriter("assignment1.txt");
+			 output.write(data);
+			 output.close();
+			 if(file.exists()) {
+					System.out.println("file created");
+				}else {
+					System.out.println("file already exist");
+				}
+		      FileReader input = new FileReader("assignment1.txt");
+		      input.read(array);
+		      input.close();
+		      System.out.println("Data in the file:");
+		      System.out.println(array);
+		       System.out.println("Data is written to the file.");
+			
+			
+			
+		}catch(Exception e){
+			e.getStackTrace();
+		}
+
+	}
+
+}
